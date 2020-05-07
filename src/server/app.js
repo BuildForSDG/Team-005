8fbd61328e0e9369f880d04f84a4a93f65121dfc
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-05-07 23:13:58 +0100

    code format

--- src/server/app.js
+++ src/server/app.js
@@ -3,33 +3,30 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-const passport = require('passport')
+const passport = require('passport');
 var Router = require('./routes/route');
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 
 var app = express();
 
-
-
 //map global promise - get rid of warning
-mongoose.promise=global.promise;
-mongoose.connect( 'mongodb://localhost/BDG',
-  
-{useNewUrlParser:true,
-  useUnifiedTopology: true 
-})
-.then(()=> console.log(' DB connected'))
-.catch((err) => console.log(err));
+mongoose.promise = global.promise;
+mongoose
+  .connect(
+    'mongodb://localhost/BDG',
 
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log(' DB connected'))
+  .catch((err) => console.log(err));
 
+app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs')
+app.use(morgan('dev'));
 
-app.use(morgan('dev'))
-
-app.use(passport.initialize())
-require("./config/passport")(passport)
+app.use(passport.initialize());
+require('./config/passport')(passport);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -40,11 +37,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', Router);
 
-
 var port = process.env.PORT || 5000;
 
-
 // module.exports = app;
-app.listen(port,(req,res,next)=>{
-  console.log(`server running at port ${port}`)
+app.listen(port, (req, res, next) => {
+  console.log(`server running at port ${port}`);
 });
--- src/server/models/users.js
+++ src/server/models/users.js
@@ -2,39 +2,34 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userAdminSchema = new Schema({
-    userName:{
-        type:String,
-        required:true
+  userName: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  userType: {
+    type: String,
+    required: true
+  },
 
-    },
-    firstName:{
-        type:String,
-        required:true
-
-    },
-    lastName:{
-        type:String,
-        required:true
-
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-
-        },
-        userType:{
-            type:String,
-        required:true
-
-            },
-   
-  date:{
-      type:Date,
-      default:Date.now
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
 
